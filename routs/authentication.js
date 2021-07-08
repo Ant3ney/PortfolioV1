53c7ccd9ef9000490b86cdd6df84d8944ae0067a
Anthony Cavuoti <anthonycavuoti@gmail.com>
2021-07-07 18:10:09 -0700

    Fix email system

--- routs/authentication.js
+++ routs/authentication.js
@@ -1,179 +1,197 @@
 //authentication routs
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const passport = require("passport");
-const passportCofig = require("../config/passport-setup");
+const passport = require('passport');
+const passportCofig = require('../config/passport-setup');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-var User = require("../models/users");
-var utilities = require("../middleware/utilities");
+var User = require('../models/users');
+var utilities = require('../middleware/utilities');
 
 //Set authentication permissions
 var authenticationPermission = {
 	google: true,
 	facebook: true,
-	github: true
-}
+	github: true,
+};
 
 //Get authentication views
-router.get("/login", (req, res) => {
+router.get('/login', (req, res) => {
 	console.log(req.flash('failureFlash'));
-	res.render("authentication/login", {allow: authenticationPermission});
+	res.render('authentication/login', { allow: authenticationPermission });
 });
-router.get("/signup", (req, res) => {
-	res.render("authentication/signUp", {allow: authenticationPermission});
+router.get('/signup', (req, res) => {
+	res.render('authentication/signUp', { allow: authenticationPermission });
 });
 
 //Invalid credentials router
-router.get("/invalid", (req, res) => {
-	req.flash("error", "username or password is invalid");
-	res.redirect("/");
+router.get('/invalid', (req, res) => {
+	req.flash('error', 'username or password is invalid');
+	res.redirect('/');
 });
 
 //Local sign up
-router.post("/signup", (req, res, next) => {
-	//geting credentials from view form
-	var username = req.body.username;
-	var name = req.body.namefeild;
-	var password = req.body.password;
-	
-	//Check if user exzists
-	User.find({}, (err, users) => {
-		//loop through all users and find out if incoming is dupe
-		var dupeUserIndex = utilities.findIndexOfLocalUser(users, username);
-		
-		//if username already exzists
-		if(dupeUserIndex >= 0){
-			console.log("User already exzists");
-			next();
-		}
-		
-		//create new user
-		else{
-			console.log("New local user created");
-			
-			//hashing password
-			bcrypt.hash(password, saltRounds, (err, hash) => {
-				//Create user
-				User.create(utilities.assembleLocalUser(username, name, hash), (err, user) => {
-					if(err){
-						console.log("Something went wrong");
-						console.log(err.message);
-					}
-					else{
-						console.log("Created new local user");
-						req.flash("authentication", "You have successfull logged in as " + user.name);
-						next();
-					}
+router.post(
+	'/signup',
+	(req, res, next) => {
+		//geting credentials from view form
+		var username = req.body.username;
+		var name = req.body.namefeild;
+		var password = req.body.password;
+
+		//Check if user exzists
+		User.find({}, (err, users) => {
+			//loop through all users and find out if incoming is dupe
+			var dupeUserIndex = utilities.findIndexOfLocalUser(users, username);
+
+			//if username already exzists
+			if (dupeUserIndex >= 0) {
+				console.log('User already exzists');
+				next();
+			}
+
+			//create new user
+			else {
+				console.log('New local user created');
+
+				//hashing password
+				bcrypt.hash(password, saltRounds, (err, hash) => {
+					//Create user
+					User.create(utilities.assembleLocalUser(username, name, hash), (err, user) => {
+						if (err) {
+							console.log('Something went wrong');
+							console.log(err.message);
+						} else {
+							console.log('Created new local user');
+							req.flash('authentication', 'You have successfull logged in as ' + user.name);
+							next();
+						}
+					});
 				});
-			});
-		}
-	});
-}, passport.authenticate('local', { 
+			}
+		});
+	},
+	passport.authenticate('local', {
 		successRedirect: '/',
 		failureRedirect: '/invalid',
-   		failureFlash: true
+		failureFlash: true,
 	})
 );
 
 //local login
-router.post("/login", passport.authenticate('local', {
-   		failureRedirect: '/invalid',
-   		failureFlash: true
-}), (req, res) => {
-	req.flash("authentication", "You have successfull logged in as " + req.user.name);
-	res.redirect("/");
-});
+router.post(
+	'/login',
+	passport.authenticate('local', {
+		failureRedirect: '/invalid',
+		failureFlash: true,
+	}),
+	(req, res) => {
+		req.flash('authentication', 'You have successfull logged in as ' + req.user.name);
+		res.redirect('/');
+	}
+);
 
 //auththentication logout
-router.get("/logout", (req, res) => {
-	console.log("User logged out");
- 	req.logout();
-	req.flash("authentication", "You successfully logged out")
- 	res.redirect('/');
+router.get('/logout', (req, res) => {
+	console.log('User logged out');
+	req.logout();
+	req.flash('authentication', 'You successfully logged out');
+	res.redirect('/');
 });
 
 //Authentication with google - Start
 
 //Sign up with google
-router.get("/register/google", (req, res) => {
+router.get('/register/google', (req, res) => {
 	//handle app logic before authenticating
-	res.redirect("/auth/google");
+	res.redirect('/auth/google');
 });
 
 //Login with google
-router.get("/login/google", (req, res) => {
+router.get('/login/google', (req, res) => {
 	//handle app login logic here
-	res.redirect("/auth/google");
+	res.redirect('/auth/google');
 });
 
 //Authenticate with google
-router.get("/auth/google", passport.authenticate("google", {
-	scope: ["profile"]
-}));
-router.get("/google/redirect", passport.authenticate("google", {
-   failureRedirect: '/invalid',
-   failureFlash: true
-}), (req, res) => {
-	req.flash("authentication", "You have successfull logged in as " + req.user.name);
-	res.redirect("/");
-});
+router.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile'],
+	})
+);
+router.get(
+	'/google/redirect',
+	passport.authenticate('google', {
+		failureRedirect: '/invalid',
+		failureFlash: true,
+	}),
+	(req, res) => {
+		req.flash('authentication', 'You have successfull logged in as ' + req.user.name);
+		res.redirect('/');
+	}
+);
 
 //Authentication with google - End
 
 //Authentication with facebook - Start
 
 //Sign up with facebook
-router.get("/register/facebook", (req, res) => {
+router.get('/register/facebook', (req, res) => {
 	//handle app logic before authenticating
-	res.redirect("/auth/facebook");
+	res.redirect('/auth/facebook');
 });
 
 //Login with facebook
-router.get("/login/facebook", (req, res) => {
+router.get('/login/facebook', (req, res) => {
 	//handle app login logic here
-	res.redirect("/auth/facebook");
+	res.redirect('/auth/facebook');
 });
 
 //Authenticate with facebook
-router.get('/auth/facebook',
-  passport.authenticate('facebook'));
-router.get("/facebook/redirect", passport.authenticate("facebook", {
-   failureRedirect: '/invalid',
-   failureFlash: true
-}), (req, res) => {
-	req.flash("authentication", "You have successfull logged in as " + req.user.name);
-	res.redirect("/");
-});
+router.get('/auth/facebook', passport.authenticate('facebook'));
+router.get(
+	'/facebook/redirect',
+	passport.authenticate('facebook', {
+		failureRedirect: '/invalid',
+		failureFlash: true,
+	}),
+	(req, res) => {
+		req.flash('authentication', 'You have successfull logged in as ' + req.user.name);
+		res.redirect('/');
+	}
+);
 
 //Authentication with facebook - End
 
 //Authentication with github - Start
 
 //login with github
-router.get("/login/github", (req, res) => {
+router.get('/login/github', (req, res) => {
 	//handle app login logic here
-	res.redirect("/auth/github");
+	res.redirect('/auth/github');
 });
 
 //Sign up with github
-router.get("/register/github", (req, res) => {
+router.get('/register/github', (req, res) => {
 	//handle app sign up logic here
-	res.redirect("/auth/github");
+	res.redirect('/auth/github');
 });
 
 //Authenticate with github
 router.get('/auth/github', passport.authenticate('github'));
-router.get('/github/redirect', 
-  passport.authenticate('github', {
-   failureRedirect: '/invalid',
-   failureFlash: true
-}),
-  function(req, res) {
-    // Successful authentication, redirect home.
-	req.flash("authentication", "You have successfull logged in as " + req.user.name);
-    res.redirect('/');
-  });
+router.get(
+	'/github/redirect',
+	passport.authenticate('github', {
+		failureRedirect: '/invalid',
+		failureFlash: true,
+	}),
+	function (req, res) {
+		// Successful authentication, redirect home.
+		req.flash('authentication', 'You have successfull logged in as ' + req.user.name);
+		res.redirect('/');
+	}
+);
 
 //Authentication with github - End
 
--- routs/index.js
+++ routs/index.js
@@ -1,55 +1,85 @@
 //index routs
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var skillsRoute = require("./skill");
-var projectsRoute = require("./project");
-var SkillCat = require("../models/skillCat");
-var Project = require("../models/project");
+var skillsRoute = require('./skill');
+var projectsRoute = require('./project');
+var SkillCat = require('../models/skillCat');
+var Project = require('../models/project');
 const Experience = require('../models/experience');
 const sendmail = require('sendmail')();
+const fetch = require('node-fetch');
 
 //Adding routs
 skillsRoute(router);
 projectsRoute(router);
 
 //Show landing page
-router.get("/", (req, res) => {
-	
-	Promise.all([
-		SkillCat.find({}),
-		Project.find({}),
-		Experience.find({})
-	])
-	.then(dbEntries => {
-		let skillCat = dbEntries[0];
-		let projects = dbEntries[1];
-		let experiences = dbEntries[2];
-		res.render("index", {skillCats: skillCat, projects: projects, experiences: experiences, settings: {
-			startShowingAll: false
-		}});
-	})
-	.catch(err => {
-		res.status(500).send(`failed to load index page becaiuse ${err.message}`);
-	});
-});
-
-router.post("/contactMe", (req, res) => {
-	var message = req.body.message.toString() + "\n" + "From: " + req.body.email.toString();
-	 sendmail({
-			from: 'portfoliomail@gmail.com',
-			to: 'anthonycavuoti@gmail.com',
-			subject: 'JOB OPPORTUNITY LOOK HERE',
-			html: message,
-		  	}, function(err, reply) {
-				console.error(err && err.stack);
-				console.dir(reply);
-		 		res.redirect("/");
+router.get('/', (req, res) => {
+	Promise.all([SkillCat.find({}), Project.find({}), Experience.find({})])
+		.then(dbEntries => {
+			let skillCat = dbEntries[0];
+			let projects = dbEntries[1];
+			let experiences = dbEntries[2];
+			res.render('index', {
+				skillCats: skillCat,
+				projects: projects,
+				experiences: experiences,
+				settings: {
+					startShowingAll: false,
+				},
+			});
+		})
+		.catch(err => {
+			res.status(500).send(`failed to load index page becaiuse ${err.message}`);
 		});
 });
 
-//test send file
-router.get("/download", (req, res) => {
-	res.download(__dirname + "/../downloads/ResumeAnthonyCavuotifullstackwebdeveloper.pdf");	
+router.post('/contactMe', (req, res) => {
+	let emailServerOrgin = 'https://mail-server-system.herokuapp.com';
+	let fromEmail = req.body.email.toString() || 'un specified email';
+	var message = `${req.body.message.toString()}\nFrom: ${fromEmail}`;
+
+	if (process.env.PRODUCTION === 'dev') {
+		emailServerOrgin = 'http://localhost:3002';
+	}
+	let body = JSON.stringify({
+		message: message,
+		from: fromEmail,
+		subject: 'Portfolio Mail',
+	});
+
+	fetch(`${emailServerOrgin}/sendMail`, {
+		method: 'post',
+		body: body,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then(res => {
+			return res.json();
+		})
+		.then(status => {
+			console.log(status);
+			res.redirect('/sucessfullEmail');
+		})
+		.catch(err => {
+			console.error(err);
+			res.redirect('/failedEmail');
+		});
+});
+
+router.get('/sucessfullEmail', (req, res) => {
+	req.flash('authentication', 'You have sucessfully sent me an email');
+	res.redirect('/');
+});
+
+router.get('/failedEmail', (req, res) => {
+	req.flash('error', 'email failed to send');
+	res.redirect('/');
+});
+
+router.get('/download', (req, res) => {
+	res.download(__dirname + '/../downloads/ResumeAnthonyCavuotifullstackwebdeveloper.pdf');
 });
 
 module.exports = router;
