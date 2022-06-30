ab4fa55164c7f5205d18695634fd39fa405ce45f
Anthony Cavuoti <anthonycavuoti@gmail.com>
2022-06-29 20:11:47 -0700

    add basic views to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Header />
+			<div></div>
+		</div>
+	);
+}
+
+function LandingScreen() {
+	return <div className=''></div>;
+}
+
+function Header() {
+	return (
+		<header className='flex flex-row w-screen'>
+			<nav className='flex w-full portfolio-container'>
+				<ul className='mr-auto flex'>
+					<li className='h-full w-auto p-6'>
+						<a href='#'>Home</a>
+					</li>
+				</ul>
+				<ul>
+					<li className='h-full w-auto p-6'>
+						<a href='#'>Download Resume</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default App;
